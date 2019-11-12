c13fb0b8215d9b23836aa1b31208209d06123011
aperuchena <aimarperuchena@gmail.com>
2019-11-12 22:22:35 +0100

    subir archivos

---
+++ proyecto/resources/views/inicio.blade.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/estilo.css">
+
+    <title>Hello, world!</title>
+</head>
+
+<body>
+    <div class="container col-12">
+        <div class=" row header">
+            <div class="col-4 titulo">
+                <p>Gastro Society</p>
+            </div>
+            <div class="col-4 icono">
+                <p>Icono</p>
+            </div>
+            <div class="col-4 login">
+                <p>Login</p>
+            </div>
+        </div>
+        <div class="row fondo">
+            <div class="div_eslogan row">
+                <p class="eslogan">Elijanos para su sociedad</p>
+            </div>
+
+            <div class="div_datos_ingresar row">
+            <input type="text" class="form-control" aria-label="Amount (to the nearest dollar)">
+            </div>
+
+        </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="js/jquery-3.3.1.slim.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
